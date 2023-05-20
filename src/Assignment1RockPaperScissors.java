555ae465a0aae3541931b147ec7d78865717c817
JamesyLive <jamesjrbus@gmail.com>
2023-05-19 20:53:21 -0400

    Debugged and works properly.

--- src/Assignment1RockPaperScissors.java
+++ src/Assignment1RockPaperScissors.java
@@ -3,17 +3,18 @@ public class Assignment1RockPaperScissors {
     public static void main(String[] args) {
         String playerOne = "";
         String playerTwo = "";
-        String playAgain = "Y";
         boolean done = false;
+        String playAgain = "";
+        boolean play = true;
 
         Scanner in = new Scanner(System.in);
         String error = "";
-        do {
+        while (play == true)
+        {
             do {
                 System.out.println("Player 1 move (R/P/S): ");
                 if (in.hasNextLine()) {
                     playerOne = in.nextLine();
-                    in.hasNextLine();
                     done = true;
                 } else {
                     error = in.nextLine();
@@ -26,7 +27,6 @@ public class Assignment1RockPaperScissors {
                 System.out.println("Player 2 move (R/P/S): ");
                 if (in.hasNextLine()) {
                     playerTwo = in.nextLine();
-                    in.hasNextLine();
                     done = true;
                 } else {
                     error = in.nextLine();
@@ -37,58 +37,58 @@ public class Assignment1RockPaperScissors {
             if (playerOne.equalsIgnoreCase("R")) {
                 if (playerTwo.equalsIgnoreCase("R")) {
                     System.out.println("R X R is a tie.");
-                    System.out.println("Play again?(Y/N): ");
-                    playAgain = in.nextLine();
-                    in.hasNextLine();
                 } else if (playerTwo.equalsIgnoreCase("S")) {
                     System.out.println("R X S player 1 wins.");
-                    System.out.println("Play again?(Y/N): ");
-                    playAgain = in.nextLine();
-                    in.hasNextLine();
-                } else {
+                } else if (playerTwo.equalsIgnoreCase("P")) {
                     System.out.println("R X P player 2 wins.");
-                    System.out.println("Play again?(Y/N): ");
-                    playAgain = in.nextLine();
-                    in.hasNextLine();
+                }
+                else
+                {
+                    System.out.println("Player 1 has Rock. Player 2 input is an error.");
                 }
 
             } else if (playerOne.equalsIgnoreCase("P")) {
                 if (playerTwo.equalsIgnoreCase("R")) {
                     System.out.println("P X R player 1 wins.");
-                    System.out.println("Play again?(Y/N): ");
-                    playAgain = in.nextLine();
-                    in.hasNextLine();
                 } else if (playerTwo.equalsIgnoreCase("S")) {
                     System.out.println("P X S player 2 wins.");
-                    System.out.println("Play again?(Y/N): ");
-                    playAgain = in.nextLine();
-                    in.hasNextLine();
-                } else {
+                } else if (playerTwo.equalsIgnoreCase("P")) {
                     System.out.println("P X P is a tie.");
-                    System.out.println("Play again?(Y/N): ");
-                    playAgain = in.nextLine();
-                    in.hasNextLine();
                 }
-            } else {
+                else
+                {
+                    System.out.println("Player 1 has Paper. Player 2 input is an error.");
+                }
+            } else if (playerOne.equalsIgnoreCase("S"))
+            {
                 if (playerTwo.equalsIgnoreCase("R")) {
                     System.out.println("S X R player 2 wins.");
-                    System.out.println("Play again?(Y/N): ");
-                    playAgain = in.nextLine();
-                    in.hasNextLine();
                 } else if (playerTwo.equalsIgnoreCase("S")) {
                     System.out.println("S X S it is a tie.");
-                    System.out.println("Play again?(Y/N): ");
-                    playAgain = in.nextLine();
-                    in.hasNextLine();
-                } else {
+                } else if (playerTwo.equalsIgnoreCase("P")) {
                     System.out.println("S X P Player 1 wins.");
-                    System.out.println("Play again?(Y/N): ");
-                    playAgain = in.nextLine();
-                    in.hasNextLine();
+                }
+                else
+                {
+                    System.out.println("Player 1 has Rock. Player 2 input is an error.");
                 }
             }
+            else
+            {
+                System.out.println("Error in player 1 input.");
+            }
+            System.out.println("Play again?(Y/N): ");
+            playAgain = in.nextLine();
+            if (playAgain.equalsIgnoreCase("Y"))
+            {
+                play = true;
+            }
+            else
+            {
+                play = false;
+            }
         }
-        while (playAgain.equalsIgnoreCase("Y"));
+
     }
 }
 
